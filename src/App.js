fad672714ccda1a1eea82b01c535eebbae3baaec
dlauck92 <drewlauck92@gmail.com>
2019-03-19 09:40:40 -0400

    updating app

---
+++ src/App.js
@@ -0,0 +1,9 @@
+//imports dependencies and files
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import Jumbotron from "./components/Jumbotron";
+import FriendCard from "./components/FriendCard";
+import Footer from "./components/Footer";
+import pokemon from "./pokemon.json";
+import "./App.css";
+
